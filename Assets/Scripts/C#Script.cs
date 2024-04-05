5c9c1f1d73f04c248d019f46cd606287c6ccd05f
Ivanstarterprog <ivan.nurgazin@yandex.ru>
2024-04-06 02:22:28 +0700

    Update C#Script.cs

--- Assets/Scripts/C#Script.cs
+++ Assets/Scripts/C#Script.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class CScript : MonoBehaviour
 {
     public int _intValue;
+    public float _floatValue;
+    public double _doubleValue;
+    public string _stringValue;
+    public bool _booleanValue;
+    public char _charValue;
     public List<int> _list;
     // Start is called before the first frame update
     void Start()
     {
         _intValue = 5;
+        _floatValue = 5f;
+        _doubleValue = 5.0;
+        _stringValue = "HelloWorld";
+        _booleanValue = true;
+        _charValue = 'K';
         _list = new List<int>();
+        Debug.Log(_intValue);
+        Debug.Log(_floatValue);
+        Debug.Log(_doubleValue);
+        Debug.Log(_stringValue);
+        Debug.Log(_booleanValue);
+        Debug.Log(_charValue);
+        Debug.Log(_list[0]);
     }
 
     // Update is called once per frame

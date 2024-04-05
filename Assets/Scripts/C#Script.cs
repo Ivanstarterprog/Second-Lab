8df93c839b63132177c7e7a439c47e704e43e20d
Ivanstarterprog <ivan.nurgazin@yandex.ru>
2024-04-06 02:21:20 +0700

    Добавлен код программы

---
+++ Assets/Scripts/C#Script.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CScript : MonoBehaviour
+{
+    public int _intValue;
+    public List<int> _list;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _intValue = 5;
+        _list = new List<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
